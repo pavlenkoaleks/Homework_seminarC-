7b450e9cf810670bb4be41b19dda059d0a209d8d
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-31 10:10:19 +0500

    задача10

--- honework2/задача10/Program.cs
+++ honework2/задача10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int N;
+N = int.Parse(Console.ReadLine());
+int A = N % 100 ;
+int B = N %10 ;
+int number = (A - B)/10;
+Console.Write($"{number}");
