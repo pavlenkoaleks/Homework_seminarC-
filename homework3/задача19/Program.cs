c7e74f8a7305b24d3a9fb5ae489903322da1f9ac
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-31 15:50:37 +0500

    задача19

--- homework3/задача19/Program.cs
+++ homework3/задача19/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number;
+number = int.Parse(Console.ReadLine());
+if((number<100000&&number>9999)||(number>-100000&&number<-9999)){
+
+int A = number/10000;
+int B=(number%10000)/1000;
+int D =(number%100)/10;
+int E = number%10;
+if((A==E)&&(B==D)) {Console.WriteLine("Это палиндром!");}
+else{Console.WriteLine("Это не палиндром!");} }
+
+else{Console.WriteLine("Неправильный ввод числа");}
+
+
+
