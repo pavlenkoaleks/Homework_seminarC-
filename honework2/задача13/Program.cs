90bbadf59791ddbbee107c2d7c85e72e3f1ea504
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-03-31 10:33:20 +0500

    задача13

---
+++ honework2/задача13/Program.cs
@@ -0,0 +1,23 @@
+﻿ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+
+//78 -> третьей цифры нет
+
+//32679 -> 6
+
+Console.Clear();
+Console.Write("Введите  число: ");
+int N;
+N = int.Parse(Console.ReadLine());
+int A = N / 100 ;
+
+
+
+if (A==0) {Console.Write("Третьей цифры нет");}
+    else{ 
+        while (N>999) {
+       N= N/10;
+    }}
+int result = N%10;
+Console.WriteLine(result);
