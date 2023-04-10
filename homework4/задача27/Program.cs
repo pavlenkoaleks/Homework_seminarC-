e9cdbfa4eac340b8a349a99fc9f93702a4608e6e
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-04-10 09:12:28 +0500

    задача 27

---
+++ homework4/задача27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int sum(int a){
+    int result=0;
+    for(int i=1;a!=0;i++){
+        result=result+a%10;
+        a=a/10;
+    }
+return result;
+}
+
+Console.Clear();
+Console.Write("Введите число : ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine(sum(A));
