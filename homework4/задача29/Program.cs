7757c95a070f3c4e891a0af73142e4d9a7efcd62
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-04-10 09:28:13 +0500

    задача 29

---
+++ homework4/задача29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+int[] array(int size){
+    int[] result = new int[size];
+    for(int i=0;i<size;i++){
+        result[i]=new Random().Next(1,1000);
+        
+    }
+return result;
+}
+
+Console.Clear();
+Console.Write("Введите число : ");
+int size = int.Parse(Console.ReadLine());
+int[] array1 = array(size);
+
+Console.WriteLine($"[   {String.Join(", ",array1)}   ]");
