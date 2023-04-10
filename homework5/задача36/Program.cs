c4c31b48671878d24a7fabddbbb02ee6781e43fb
ALEKSANDR <pavlenkoaleksandr270397@gmail.com>
2023-04-10 19:16:45 +0500

    36

--- homework5/задача36/Program.cs
+++ homework5/задача36/Program.cs
@@ -2,7 +2,7 @@
 int[] new_array(int size){
     int[] result = new int[size];
     for(int i=0;i<size;i++){
-        result[i]=new Random().Next(1,100);
+        result[i]=new Random().Next(-100,100);
         
     }
 return result;
